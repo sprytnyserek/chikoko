11555a466c95e1fb5801ad6c038a0152547f140a
sprytnyserek <sprytnyserek@bd72aad3-be2f-0410-91f6-699706c7f909>
2007-05-06 17:15:16 +0000

    otwarcie projektu, zarys pierwszych modułów

---
+++ trunk/main.d
@@ -0,0 +1,34 @@
+﻿/*
+chikoko - quadratic forms solving
+Copyright (C) 2007 Tomasz Polachowski, sprytnyserek@gmail.com
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+/**
+ * Moduł główny
+
+ * Authors: Tomasz Polachowski, $(LINK2 mailto:sprytnyserek@gmail.com,sprytnyserek@gmail.com)
+ * License: GNU General Public License 2.0, $(LINK http://www.fsf.org/licensing/licenses/gpl.html)
+ * Version: 0.0.1
+*/
+module main;
+
+import std.stdio;
+import lattice.reprezentation;
+
+int main(char[][] args) {
+writefln("Hello:[");
+return 0;
+}
