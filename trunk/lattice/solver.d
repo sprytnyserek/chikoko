bbfbe9e1e52303bfb431cb3187ef740220936d8c
sprytnyserek <sprytnyserek@bd72aad3-be2f-0410-91f6-699706c7f909>
2007-05-16 19:15:06 +0000

    dodano gui.winmain, zmieniono lattice.solver

--- trunk/lattice/solver.d
+++ trunk/lattice/solver.d
@@ -25,6 +25,56 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 module lattice.solver;
 
-private import lattice.reprezentation;
+//private import lattice.reprezentation;
+private import std.math;
 
 
+/**
+ * Sprawdza typ Sylvestera (określoność) formy kwadratowej 
+ * reprezentowanej przez macierz symetryczną (ale macierz wejściowa 
+ * jest traktowana jak macierz dolnotrójkątna) z użyciem 
+ * zmodyfikowanej eliminacji Gaussa.
+ * 
+ * Params: 
+ * 	bq = 	dolnotrójkątna macierz Grama formy kwadratowej
+ * Returns: kod określoności formy kwadratowej:$(BR) 
+ *  $(B -2) - forma ujemnie określona, $(BR)
+ *  $(B -1) - forma niedodatnio określona, $(BR)
+ * 	 $(B 0) - $(B$(RED forma nieokreślona)), $(BR)
+ *   $(B 1) - forma nieujemnie określona, $(BR)
+ *   $(B 2) - forma dodatnio określona.
+ */
+byte getSylvesterType(long[][] bq) {
+uint n = bq.length;
+double[][] m;
+uint i,j;
+byte r = 2; //typ Sylvestera
+
+m.length = n;
+for (i = 0; i < n; i++) {
+	m[i].length = i + 1;
+	for (j = 0; j <= i; j++) {
+		m[i][j] = bq[i][j];
+		}
+	}
+for (i = 0; i < n; i++) {
+	if (i == 0) {
+		if (m[0][0] == 0) r = 1;
+		if (m[0][0] < 0) r = -2;
+		}
+	else {
+		if (m[i][i] < 0) return 0;
+		if ((m[i][i] == 0) && (abs(cast(int)r) == 2)) r /= 2;
+		}
+	if (m[i][i] == 0) {
+		/* przewartościowanie minora na pomocą operacji elementarnej
+		 * niezmienniczej względem wyznacznika całej macierzy; 
+		 * ta operacja zmienia minory(!), ale jest wykonywana tylko w przypadku
+		 * wykluczenia dodatniej i ujemnej określoności */
+		
+		/* powtórzenie testu na określoność ze względu na modyfikację minora */
+		
+		}
+	}
+return r;
+}
